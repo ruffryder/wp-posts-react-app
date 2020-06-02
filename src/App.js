5a0583487d13eff8a4a12a18eb97e79d05099f11
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-06-02 10:40:53 +0300

    Adding LoadingSpinner component and showing it on new posts fetch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from "./components/Header/Header";
 import PostsList from "./components/PostsList/PostsList";
 import Button from "./components/UI/Button/Button";
 import Footer from "./components/Footer/Footer";
+import LoadingSpinner from "./components/UI/LoadingSpinner/LoadingSpinner";
+
 /* eslint-disable react/no-unused-state, no-console */
 class App extends Component {
   constructor(props) {
@@ -12,66 +14,76 @@ class App extends Component {
     this.state = {
       posts: [],
       postsOffset: constants.POSTS_OFFSET,
-      noPostsLeft: false,
+      showLoadMorePostsButton: true,
+      isLoading: false,
     };
   }
 
   componentDidMount() {
     const { postsOffset } = this.state;
+    this.setState({
+      showLoadMorePostsButton: false,
+      isLoading: true,
+    });
     axios
       .get(
         `https://renemorozowich.com/wp-json/wp/v2/posts?_embed&per_page=${constants.POSTS_PER_PAGE}&offset=${postsOffset}`,
       )
       .then((res) => {
-        // Check if the initial result is less than our POSTS_PER_PAGE constant
-        let noPostsLeft = false;
-        if (res.data.length < constants.POSTS_PER_PAGE) {
-          noPostsLeft = true;
-        }
+        /* Check how many posts were returned and
+        show Load more posts button only if there are more left */
+        const returnedPosts = res.data.length;
         this.setState((prevState) => ({
           posts: res.data,
           postsOffset: prevState.postsOffset + constants.POSTS_OFFSET,
-          noPostsLeft,
+          showLoadMorePostsButton: returnedPosts === constants.POSTS_PER_PAGE,
+          isLoading: false,
         }));
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        this.setState({ isLoading: false });
+      });
   }
 
   handleLoadMorePosts = () => {
     const { postsOffset } = this.state;
+    this.setState({ showLoadMorePostsButton: false, isLoading: true });
     axios
       .get(
         `https://renemorozowich.com/wp-json/wp/v2/posts?_embed&per_page=${constants.POSTS_PER_PAGE}&offset=${postsOffset}`,
       )
       .then((res) => {
-        // Check if the returned result is less than our POSTS_PER_PAGE constant
-        let noPostsLeft = false;
-        if (res.data.length < constants.POSTS_PER_PAGE) {
-          noPostsLeft = true;
-        }
+        /* Check how many posts were returned and
+      show Load more posts button only if there are more left */
+        const returnedPosts = res.data.length;
         this.setState((prevState) => ({
           posts: [...prevState.posts, ...res.data],
           postsOffset: prevState.postsOffset + constants.POSTS_OFFSET,
-          noPostsLeft,
+          showLoadMorePostsButton: returnedPosts === constants.POSTS_PER_PAGE,
+          isLoading: false,
         }));
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        this.setState({ isLoading: false });
+      });
   };
 
   render() {
-    const { posts } = this.state;
-    const { noPostsLeft } = this.state;
+    const { posts, showLoadMorePostsButton, isLoading } = this.state;
     return (
       <>
         <div className="container">
           <Header />
           <PostsList posts={posts} />
-          {noPostsLeft ? null : (
+          {showLoadMorePostsButton ? (
             <Button
               textValue="Load more posts"
               clickHandler={this.handleLoadMorePosts}
             />
-          )}
+          ) : null}
+          {isLoading ? <LoadingSpinner /> : null}
         </div>
         <Footer />
       </>
